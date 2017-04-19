e38b2c6f30c8c05332dba627159d86e18cf5451d
Nima <nimaseyf@gmail.com>
2017-04-19 14:28:05 +0100

    Added comments

--- js/index.js
+++ js/index.js
@@ -5,9 +5,9 @@ $('#randomArticle').click(function () {
 });
 
 $('#search').click(function () {
-
+// Assigns the user's input string to a variable
   var input = $('#userInput').val();
-
+// Queries the wikipedia API using the user input string
   var url = "https://en.wikipedia.org/w/api.php?action=opensearch&search=" + input + "&format=json&smaxage=0&callback=?&profile=fuzzy";
 
   $.ajax({
@@ -17,6 +17,7 @@ $('#search').click(function () {
     dataType: "json",
     success: function success(data) {
       $('#results').html("");
+ // Populates the page with <li> elements for each corresponding wikipedia article
       for (var i = 0; i < data[1].length; i++) {
         $('#results').append("<li><a href= " + data[3][i] + ">" + data[1][i] + "</a><p>" + data[2][i] + "</p></li>");
       }
